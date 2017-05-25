7f0b8c578fe0a9e981451f7519b42c05ce51899b
guowang.sun <guowang.sun@qunar.com>
2017-05-25 20:18:27 +0800

    bug fix

--- grab.py
+++ grab.py
@@ -21,11 +21,12 @@ def grab_goods_and_save(scan_config, db_config):
     goods_price = GoodsPrice(scan_config.j_id, price_json['op'], price_json['m'], datetime.datetime.utcnow())
     db = MySQLdb.connect(db_config.host, db_config.user, db_config.password, db_config.db)
     cursor = db.cursor()
-    insert_sql = 'INSERT INTO goods_price (goods_id, current_price, price, create_time) VALUES (%s, %s, %s, %s)' % (goods_price.goods_id,
-                                                                                                                    goods_price.current_price,
-                                                                                                                    goods_price.price,
-                                                                                                                    goods_price.create_time
-                                                                                                                    .strftime('%Y-%m-%d %H:%M:%S'))
+    insert_sql = 'INSERT INTO goods_price (goods_id, current_price, price, create_time) VALUES ("%s", %s, %s, "%s")' % (goods_price.goods_id,
+                                                                                                                        goods_price.current_price,
+                                                                                                                        goods_price.price,
+                                                                                                                        goods_price.create_time
+                                                                                                                        .strftime(
+                                                                                                                            '%Y-%m-%d %H:%M:%S'))
     print ('start insert price %s' % goods_price)
     try:
         cursor.execute(insert_sql)
@@ -34,7 +35,7 @@ def grab_goods_and_save(scan_config, db_config):
     except Exception as e:
         print ('insert error %s' % e)
         db.rollback()
-    select_sql = 'SELECT id, j_id, name, url, comment, create_time FROM goods_message WHERE j_id = %s' % scan_config.j_id
+    select_sql = 'SELECT id, j_id, name, url, comment, create_time FROM goods_message WHERE j_id = "%s"' % scan_config.j_id
     cursor.execute(select_sql)
     result = cursor.fetchall()[0]
     db.close()
